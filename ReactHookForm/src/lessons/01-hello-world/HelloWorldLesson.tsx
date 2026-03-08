e837ed27fe126674dd75bc1b0c7d5dadfc863753
IchinaZiru <yutaihuangjing08@gmail.com>
2026-03-08 15:45:04 +0900

    RHF概念

--- ReactHookForm/src/lessons/01-hello-world/HelloWorldLesson.tsx
+++ ReactHookForm/src/lessons/01-hello-world/HelloWorldLesson.tsx
@@ -8,18 +8,30 @@
 //   [ ] onSubmit 関数でコンソールに data を表示する
 //   [ ] 送信後にコンソールで入力値が確認できる
 
-// import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  username : string
+}
 
 export default function HelloWorldLesson() {
   // useForm() を呼び出して handleSubmit と register を取り出す
-
+  const { handleSubmit, register } = useForm<FormData>()
   // onSubmit 関数を作る（引数: data）
+  const onSubmit = (data: FormData) => {
+    // data をコンソールに表示する
+    console.log(data)
+  }
 
   return (
     <div>
       <h2>Lesson 01: Hello World</h2>
       {/* <form> タグをここに作る */}
       {/* username の input と送信ボタンを追加する */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('username')} placeholder="Username" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
