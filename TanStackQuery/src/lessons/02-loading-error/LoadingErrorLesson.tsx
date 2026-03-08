c0e2734dfad38e8a668492162e11251ddf607c7f
IchinaZiru <yutaihuangjing08@gmail.com>
2026-03-08 16:48:15 +0900

    isPeding / isError

--- TanStackQuery/src/lessons/02-loading-error/LoadingErrorLesson.tsx
+++ TanStackQuery/src/lessons/02-loading-error/LoadingErrorLesson.tsx
@@ -20,13 +20,23 @@ export default function LoadingErrorLesson() {
   })
 
   // isPending のとき: ここに読み込み中UIを返す
-
+  if(isPending) {
+    return(
+      <p>読み込み中...</p>
+    )
+  }
   // isError のとき: ここにエラーUIを返す（error.message を表示する）
-
+  if(isError){
+    return(
+      <p>エラー: {error?.message}</p>
+    )
+  }
   return (
     <div className="p-8">
       <h2 className="text-2xl font-bold mb-6">Lesson 02: Loading & Error States</h2>
       {/* data がある（成功）ときのUIをここに書く */}
+      <p>Data : {data?.title}</p>
+      <p>Body : {data?.body}</p>
     </div>
   )
 }
